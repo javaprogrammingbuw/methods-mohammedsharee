cda926eece3f4b618d9ce681200f70fd96b57bf6
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-03-19 12:34:27 +0100

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 
 public class Circle {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the Radius");
+		double r = scan.nextInt();
+		
+	System.out.println(diameter (r));
+	System.out.println(area (r,3.17));
+	System.out.println(circumference (r,3.17));
+	}
+	
+	public static double diameter (double r) {
+		return r*2;
+	}
+	
+	public static double area (double r, double pi) {
+	 
+		return pi*r*r;
+	}
+	public static double circumference (double r, double pi) {
+		
+		return 2*pi*r;
+	
+	}
+	
+	
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm you methods are working right.
+
+
 }
