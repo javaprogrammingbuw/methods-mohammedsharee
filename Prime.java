af1f346de32a4f85ee375fdc6a55d03e8a115683
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-03-27 21:40:28 +0100

    updated prime

--- Prime.java
+++ Prime.java
@@ -8,31 +8,40 @@ public class Prime {
 	int a = scan.nextInt();
 	
 	System.out.println(isPrime(a));
+	System.out.println(findDoublette(a));
 	
 }
 
-	//Doublette missing!
-
 	
-	public static int isPrime (int a) {
+	public static boolean isPrime (int a) {
 	 if ( a%1==0 && a%a==0 && a%2!=0  && a%3!=0 ) {
 		 System.out.println("number is prime");
-		
-	 }else {
+		return true;
+	 }else 
+	 {
 	System.out.println("Number is not prime");	 
+	 return false;
 	 }
-	 return a;
-	 }
-		
-}		
-		
-	//todo: write a method isPrime which tells you if a given number is a prime or not
-	//then, write another method findDoublette which takes an integer value as input and 
-	//is looking for the first prime-doublette it findes. A prime-doublette consists of
-	//two prime values with the difference of 2 (e.g. 3 and 5).
-	//The function should return the smaller one of the doublette. If there exists no
-	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
-	//the function should return 0.
-		
+	}
+	
+	
+	
+	public static int findDoublette(int n) {
+
+		for(int i= n; i<Integer.MAX_VALUE; i++) {
+			boolean r = isPrime(i);
+			if(r) {
+				int j=i+2;
+				boolean r1= isPrime(j);
+				if(r1) {
+				return i;
+				}
+			}
+		}
+		return -1;
+
+	}
+	
+
 	
 
